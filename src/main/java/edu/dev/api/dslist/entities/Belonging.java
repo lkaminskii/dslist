cd7e96911645621b931b3ccfda298307ded8ba72
Lucas Kaminski <lucaskaminskith@gmail.com>
2023-05-11 15:51:35 -0300

    Domain model

---
+++ src/main/java/edu/dev/api/dslist/entities/Belonging.java
@@ -0,0 +1,50 @@
+package edu.dev.api.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+	
+	@EmbeddedId
+	private BelongingPK id = new BelongingPK();
+	private Integer position;
+	
+	public Belonging () {
+		
+	}
+
+	public Belonging(Game game, GameList gameList, Integer position) {
+		id.setGame(game);
+		id.setGameList(gameList);
+		this.position = position;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Belonging other = (Belonging) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
---
+++ src/main/java/edu/dev/api/dslist/entities/GameList.java
@@ -0,0 +1,52 @@
+package edu.dev.api.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	public GameList () {
+		
+	}
+
+	public GameList(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GameList other = (GameList) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
